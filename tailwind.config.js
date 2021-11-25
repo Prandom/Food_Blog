84e4866fe5e641b955bbe7df74f188e1382596a2
Prannoy <cprannoy@gmail.com>
2021-11-26 02:15:47 +0530

    Navbar_Draft01

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'mont':['Montserrat','sans-serif'],
+        'ubu':['Ubuntu','sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
